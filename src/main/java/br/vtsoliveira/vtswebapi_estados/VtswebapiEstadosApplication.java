64ee78bfd6143083e5b59a04c40e8570b7a59583
Vitor Souza <vtsoliveira2001@gmail.com>
2022-08-25 18:01:38 -0300

    arrumando eureka

--- src/main/java/br/vtsoliveira/vtswebapi_estados/VtswebapiEstadosApplication.java
+++ src/main/java/br/vtsoliveira/vtswebapi_estados/VtswebapiEstadosApplication.java
@@ -2,10 +2,10 @@ package br.vtsoliveira.vtswebapi_estados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class VtswebapiEstadosApplication {
 
 	public static void main(String[] args) {
