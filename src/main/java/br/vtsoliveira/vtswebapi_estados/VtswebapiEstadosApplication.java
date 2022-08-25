589841712e286fa41fb6ad6136af8a05ca64e548
Vitor Souza <vitor.souza@absoftware.com.br>
2022-08-25 00:29:16 -0300

    Subindo aplicação maven

---
+++ src/main/java/br/vtsoliveira/vtswebapi_estados/VtswebapiEstadosApplication.java
@@ -0,0 +1,13 @@
+package br.vtsoliveira.vtswebapi_estados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtswebapiEstadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtswebapiEstadosApplication.class, args);
+	}
+
+}
