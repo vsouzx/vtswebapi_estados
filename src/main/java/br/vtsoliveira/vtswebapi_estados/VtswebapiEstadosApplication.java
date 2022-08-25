d2ae06d6e8e5f4c2ff9e3f2c60fdd2cb21db11a8
Vitor Souza <vitor.souza@absoftware.com.br>
2022-08-25 15:57:20 -0300

    adicionando discovery e ajustando portas

--- src/main/java/br/vtsoliveira/vtswebapi_estados/VtswebapiEstadosApplication.java
+++ src/main/java/br/vtsoliveira/vtswebapi_estados/VtswebapiEstadosApplication.java
@@ -2,7 +2,9 @@ package br.vtsoliveira.vtswebapi_estados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class VtswebapiEstadosApplication {
 
