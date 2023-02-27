289c7cbee90bb1f761cda5bed6e00a597395b584
Vitor Souza <vitor.souza@absoftware.com.br>
2023-02-27 12:10:16 -0300

    Websocket

--- src/main/java/br/vtsoliveira/vtswebapi_estados/config/websocket/WebSocketConfig.java
+++ src/main/java/br/vtsoliveira/vtswebapi_estados/config/websocket/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package br.vtsoliveira.vtswebapi_estados.config.websocket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -10,6 +11,8 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final NotificationWebSocket meuWebSocketHandler;
+    @Value("${settings.allow-origin}")
+    private String origin;
 
     public WebSocketConfig(NotificationWebSocket meuWebSocketHandler) {
         this.meuWebSocketHandler = meuWebSocketHandler;
@@ -17,6 +20,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(meuWebSocketHandler, "/web-socket").setAllowedOriginPatterns("*");
+        registry.addHandler(meuWebSocketHandler, "/web-socket").setAllowedOriginPatterns(origin);
     }
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,4 @@
-var socket = new WebSocket("ws://localhost:8888/vtswebapiestados/web-socket");
+var socket = new WebSocket("ws://localhost:8888/web-socket");
 socket.onmessage = function(event) {
     if (event.data === "new-notification") {
         alert("nova notificação")
